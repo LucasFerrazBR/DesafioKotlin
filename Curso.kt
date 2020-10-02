d653d9905e47dbab90a1fa712c36947562239f60
LucasFerrazBR <56172744+LucasFerrazBR@users.noreply.github.com>
2020-10-02 18:57:31 -0300

    Professores e alunos adicionados

--- Curso.kt
+++ Curso.kt
@@ -1,4 +1,8 @@
-class Curso(val nome: String, val codCurso: Int) {
+class Curso(val nome: String, val codCurso: Int, val maxAlunos: Int) {
+
+    private var professorTitular: ProfessorTitular? = null
+    private var professorAdjunto: ProfessorAdjunto? = null
+    private val alunosMatriculados = mutableListOf<Aluno>()
 
     override fun equals(other: Any?): Boolean {
         if (other is Curso){
@@ -9,5 +13,13 @@ class Curso(val nome: String, val codCurso: Int) {
 
     override fun toString(): String {
         return "{Nome: ${this.nome}, Código do curso: ${this.codCurso}}"
+    }
+
+    fun setProfessorTitular(Professor: ProfessorTitular){
+        this.professorTitular = Professor
+    }
+
+    fun setProfessorAdjunto(Professor: ProfessorAdjunto){
+        this.professorAdjunto = Professor
     }
 }
