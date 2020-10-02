a00f58384ae1da98abda48512c9330b005d2f3d9
LucasFerrazBR <56172744+LucasFerrazBR@users.noreply.github.com>
2020-10-02 19:12:52 -0300

    Novas classes

---
+++ DigitalHouseManager.kt
@@ -0,0 +1,7 @@
+class DigitalHouseManager {
+
+    val cursos = mutableListOf<Curso>()
+    val professores = mutableListOf<Professor>()
+    val alunos = mutableListOf<Aluno>()
+    val matriculas = mutableListOf<Matricula>()
+}
---
+++ Matricula.kt
@@ -0,0 +1,5 @@
+import java.util.*
+
+data class Matricula(val aluno: Aluno, val curso: Curso, val data: Date) {
+
+}
